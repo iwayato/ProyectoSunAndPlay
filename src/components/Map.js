26fc7c17ea9bed3b6029c1c9e3b7ebd730e0a63a
Tomoaki Iwaya Laptop <iwayatomoaki@gmail.com>
2022-01-12 14:48:58 -0300

    Stadia Maps eliminados

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import { MapContainer, TileLayer, LayersControl, ScaleControl } from 'react-leaflet';
-//import useSwr from 'swr';
+import useSwr from 'swr';
 import Links from './Links';
 import "leaflet.heat"
 import HeatMap from './HeatMap';
 import TachasMap from './TachasMap';
 import info from './data.json'
-//import ZoomLevel from './ZoomLevel';
 
 const Map = () => {
 
+    // Centro del Mapa
     const center = [-33.033916, -71.594816];
-    //const fetcher = (...args) => fetch(...args).then(response => response.json());
-    //const url = "https://data.police.uk/api/crimes-street/all-nodo?lat=52.629729&lng=-1.131592&date=2019-10";
-    //const { data, error } = useSwr(url, { fetcher });
-    //const nodos = data && !error ? data.slice(0, 5000) : [];
+    const fetcher = (...args) => fetch(...args).then(response => response.json());
+    const url = "https://data.police.uk/api/crimes-street/all-nodo?lat=52.629729&lng=-1.131592&date=2019-10";
+    const { data, error } = useSwr(url, { fetcher });
+    const nodos = data && !error ? data.slice(0, 5000) : [];
 
     return(
 
         <MapContainer center={center} zoom={14} minZoom={11} maxZoom={17} scrollWheelZoom={true} keyboardPanDelta={200} zoomControl={false}>
-
             <ScaleControl></ScaleControl>
 
             <Links></Links>
@@ -30,17 +29,9 @@ const Map = () => {
                     <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
                 </LayersControl.BaseLayer>
 
-                {/*<LayersControl.BaseLayer name='Visual Friendly'>
-                    <TileLayer attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors' url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"/>
-                </LayersControl.BaseLayer>*/}
-
                 <LayersControl.BaseLayer name='GrayScale Map'>
                     <TileLayer attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>, under <a href="https://www.openstreetmap.org/copyright">ODbL</a>' url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.png"/>
                 </LayersControl.BaseLayer>
-
-                {/*<LayersControl.BaseLayer name='Dark Mode'>
-                    <TileLayer attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors' url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"/>                
-                </LayersControl.BaseLayer>*/}
 
                 <LayersControl.Overlay name='Tachas instaladas' checked>
                     <TachasMap nodos={info}></TachasMap>
@@ -51,8 +42,8 @@ const Map = () => {
                 </LayersControl.Overlay>
     
             </LayersControl>
-
         </MapContainer>
+
     );
 };
 export default Map;
