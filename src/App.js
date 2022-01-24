bfe5e3a7a3a31c6996a43dd8c6a8e288c694fc7b
Tomoaki Iwaya Laptop <iwayatomoaki@gmail.com>
2022-01-24 18:32:02 -0300

    Change on tachas´s color

--- src/App.js
+++ src/App.js
@@ -5,20 +5,6 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue } from "firebase/database";
 import firebaseConfig from './components/firebaseConfig';
 
-const url = 'https://54.221.23.13:8080/api/devices/2cf7f1203230a466/queue';
-const body = JSON.stringify( { "deviceQueueItem": {"confirmed": true, "data": "Ag==", "devEUI": "2cf7f1203230a466", "fCnt": 0, "fPort": 3} } );
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5X2lkIjoiNDExMzkxZDItMGY0ZC00MTAzLTkwZjAtNDE1NTQ0ZjA4YTljIiwiYXVkIjoiYXMiLCJpc3MiOiJhcyIsIm5iZiI6MTY0MjYxNjIwMCwic3ViIjoiYXBpX2tleSJ9._a7OBvSyrw0z-Gdo36W2SgjoVfe5syE2ZgKsl-13qes";
-
-fetch(url, {
-  method: 'POST',
-  mode: 'cors',
-  headers: {
-    'content-type': "application/json", 
-    'grpc-metadata-authorization': "Bearer " + token
-  },
-  body: body,
-}).then(() => console.log('Se envió información por http'));
-
 class App extends Component {
 
   constructor(props) {
@@ -35,7 +21,10 @@ class App extends Component {
     const db = getDatabase(app);
     const dbRef = ref(db, 'data/');
     const dbloc = ref(db, 'locations/');
-    
+
+    //onValue se actualiza cada vez que algun dato cambia en la base de datos de RealTime Database
+
+    //Para las posiciones de las tachas
     onValue(dbloc, (snapshot) => {
       let loc =[];
       snapshot.forEach(data => {
@@ -46,7 +35,7 @@ class App extends Component {
       this.setState({ locations: loc })
     });
 
-    //onValue se actualiza cada vez que algun dato cambia en la base de datos de RealTime Database
+    //Para los datos de las tachas
     onValue(dbRef, (snapshot) => {
       let newData = [];
       snapshot.forEach(data => {
@@ -64,7 +53,6 @@ class App extends Component {
 
   render(){
       return(
-        //infoTachas debe contener una lista con los objetos que corresponden a los datos de cada tacha
         <Map infoTachas={this.state.data} locations={this.state.locations}></Map>
       );
   }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MapContainer, TileLayer, LayersControl, ScaleControl, useMapEvent } from 'react-leaflet';
 import Links from './Links';
 import "leaflet.heat"
@@ -6,6 +6,7 @@ import HeatMap from './HeatMap';
 import TachasMap from './TachasMap';
 import HumMap from './HumMap';
 import VibMap from './VibMap';
+import ColorTacha from './ColorTachas';
 import '../Map.css';
 
 // Funcion para hacer paning de forma suave
@@ -28,46 +29,81 @@ const Map = (props) => {
     // Zoom y Centro del Mapa
     const zoom = 14;
     const center = [-33.033916, -71.594816];
+    
+    const [color, setColor] = useState('gray');
+
+    function colorTacha (color) {
+        
+        switch (color) 
+        {
+            case 'gray':
+                setColor('green')
+                break;
+            case 'green':
+                setColor('yellow')
+                break;
+            case 'yellow':
+                setColor('red')
+                break;
+            default:
+                setColor('green')
+                break;
+        }
+    }
 
     return(
 
-        <MapContainer center={center} zoom={zoom} minZoom={4} maxZoom={17} scrollWheelZoom={true} keyboardPanDelta={200} zoomControl={true}>
-        
-            <Links></Links>
+        <div>
 
-            <LayersControl position='topright' collapsed={true} className="Map">
+            <button onClick={(e) => {e.preventDefault();
+                                    colorTacha(color);                
+            }}>                         
+            Cambiar color
+            </button>
 
-                <LayersControl.BaseLayer name = 'Standart Map' checked = {true}>
-                    <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-                </LayersControl.BaseLayer>
+            <MapContainer center={center} zoom={zoom} minZoom={4} maxZoom={17} scrollWheelZoom={true} keyboardPanDelta={200} zoomControl={true}>
+            
+                <Links></Links>
 
-                <LayersControl.BaseLayer name='GrayScale Map' checked={false}>
-                    <TileLayer attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>, under <a href="https://www.openstreetmap.org/copyright">ODbL</a>' url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.png"/>
-                </LayersControl.BaseLayer>
+                <LayersControl position='topright' collapsed={false} className="Map">
 
-                <LayersControl.Overlay name='Mapa tachas instaladas' checked={true} >
-                    <TachasMap infoTachas={props.infoTachas}></TachasMap>
-                </LayersControl.Overlay>
-                
-                <LayersControl.Overlay name='Mapa de temperatura' checked={false}>
-                    <HeatMap infoTachas={props.infoTachas} zoom={zoom}></HeatMap>
-                </LayersControl.Overlay>
+                    <LayersControl.BaseLayer name = 'Standart Map' checked = {true}>
+                        <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                    </LayersControl.BaseLayer>
 
-                <LayersControl.Overlay name='Mapa de vibraciones' checked={false}>
-                    <VibMap infoTachas={props.infoTachas} zoom={zoom}></VibMap>
-                </LayersControl.Overlay>
+                    <LayersControl.BaseLayer name='GrayScale Map' checked={false}>
+                        <TileLayer attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>, under <a href="https://www.openstreetmap.org/copyright">ODbL</a>' url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.png"/>
+                    </LayersControl.BaseLayer>
 
-                <LayersControl.Overlay name='Mapa de humedad' checked={false}>
-                    <HumMap infoTachas={props.infoTachas} zoom={zoom}></HumMap>
-                </LayersControl.Overlay>
-                
-            </LayersControl>
+                    <LayersControl.Overlay name='Mapa tachas instaladas' checked={true} >
+                        <TachasMap infoTachas={props.infoTachas}></TachasMap>
+                    </LayersControl.Overlay>
+                    
+                    <LayersControl.Overlay name='Mapa de temperatura' checked={false}>
+                        <HeatMap infoTachas={props.infoTachas} zoom={zoom}></HeatMap>
+                    </LayersControl.Overlay>
 
-            <ScaleControl></ScaleControl>
+                    <LayersControl.Overlay name='Mapa de vibraciones' checked={false}>
+                        <VibMap infoTachas={props.infoTachas} zoom={zoom}></VibMap>
+                    </LayersControl.Overlay>
 
-            <SetViewOnClick/>
+                    <LayersControl.Overlay name='Mapa de humedad' checked={false}>
+                        <HumMap infoTachas={props.infoTachas} zoom={zoom}></HumMap>
+                    </LayersControl.Overlay>
 
-        </MapContainer>
+                    <LayersControl.Overlay name='Mapa color tachas' checked={false}>
+                        <ColorTacha infoTachas={props.infoTachas} zoom={zoom} color={color}></ColorTacha>
+                    </LayersControl.Overlay>
+                    
+                </LayersControl>
+
+                <ScaleControl></ScaleControl>
+
+                <SetViewOnClick></SetViewOnClick>
+
+            </MapContainer>
+
+        </div>
 
     );
 };
