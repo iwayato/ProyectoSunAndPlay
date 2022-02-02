6bb9509e6cf1a82b235d322cb9ee2ac46a74a666
Tomoaki Iwaya Laptop <iwayatomoaki@gmail.com>
2022-02-02 11:54:29 -0300

    Finished implemented PolylineMap

--- src/components/PolyLineMap.js
+++ src/components/PolyLineMap.js
@@ -1,37 +1,69 @@
-import { Polyline } from "react-leaflet"
+import MarkerClusterGroup from "react-leaflet-markercluster";
+import { Polyline, Tooltip } from "react-leaflet"
 
 const color_selector = (hum) => {
 
-    if ((0 <= hum) && (hum <= 60)) {
-        return '#00ffe1'
+    if ((0 <= hum) && (hum <= 49)) {
+        return 'green'
     }
-    if ((60.001 <= hum) && (hum <= 80)) {
-        return '#00aeff'
+    if ((50 <= hum) && (hum <= 66)) {
+        return 'yellow'
     }
-    if ((80.001 <= hum) && (hum <= 100)) {
-        return '#001aff'
+    if ((67 <= hum) && (hum <= 100)) {
+        return 'red'
     }
     else {
         return 'gray'
     }
 }
 
+const isOdd = (num) => {
+    return num % 2;
+}
+
 function PolyLineMap(props) {
+
+    console.log(props.infoTachas)
 
     let locations = [];
     let colores =[];
+    let locationsOneLine = [];
 
-    props.infoTachas.forEach(tacha => {
-        locations.push([tacha.location.latitud, tacha.location.longitud]);
-        colores.push(color_selector(tacha.humedad));
+    props.infoTachas.forEach(location => {
+        locations.push([location.location.latitud, location.location.longitud]);
+        colores.push(color_selector(location.humedad));
     });
 
-    let initialLocations = [locations[0], locations[1]];
+    if (isOdd(locations.length)) {
+        for (let i = 0; i < locations.length - 1; i++) {
+            locationsOneLine.push([locations[i], locations[i + 1]]);
+        }
+    }
+    else {
+        for (let j = 0; j < locations.length; j++) {
+            locationsOneLine.push([locations[j], locations[j + 1]]);
+        }
+    }
 
     return (
-        {
-            
-        }
+        <MarkerClusterGroup disableClusteringAtZoom={13} maxClusterRadius={60} singleMarkerMode={false}>
+            {locationsOneLine.map( location => (
+                <Polyline
+                    key={locationsOneLine.indexOf(location)}
+                    positions={location}
+                    pathOptions={{color: colores[locationsOneLine.indexOf(location)], weight: 8}}>
+                    <Tooltip direction="bottom" opacity={1} sticky = {true}>
+                        ID: {props.infoTachas[locationsOneLine.indexOf(location)].id} <br></br>
+                        Latitud: {props.infoTachas[locationsOneLine.indexOf(location)].location.latitud} <br></br>
+                        Longitud: {props.infoTachas[locationsOneLine.indexOf(location)].location.longitud} <br></br>
+                        Luz: {props.infoTachas[locationsOneLine.indexOf(location)].luz}<br></br>
+                        Temperatura: {props.infoTachas[locationsOneLine.indexOf(location)].temperatura}<br></br>
+                        Humedad: {props.infoTachas[locationsOneLine.indexOf(location)].humedad}<br></br>
+                        Acelerometro : {props.infoTachas[locationsOneLine.indexOf(location)].acelerometro} <br></br>
+                    </Tooltip>
+                </Polyline>
+            ))};
+        </MarkerClusterGroup>
     );
 };
 
