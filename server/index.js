cc52f823c61ca6bb3b76cfb0e309bca5a4942403
iwayato <iwayatomoaki@gmail.com>
2022-02-08 21:11:51 -0300

    Updated index.js server file

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'test',
+})
 
 app.get('/', (req, res) => {
-    res.send('asdasdasd');
+    const sqlInsert = "INSERT INTO data (name, info) VALUES ('cat', 'dog');"
+    db.query(sqlInsert, (err, result) => {
+        res.send('hola');
+    });
+    
 });
 
 app.listen(3001, () => {
     console.log('running on port 3001');
 });
-
