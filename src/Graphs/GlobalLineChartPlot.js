2ea55f76d7774e3063c74b64f8e436a3a92104f7
iwayato <iwayatomoaki@gmail.com>
2022-02-15 23:13:08 -0300

    Changes on Global plot

--- src/Graphs/GlobalLineChartPlot.js
+++ src/Graphs/GlobalLineChartPlot.js
@@ -1,55 +1,63 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
 import { Line } from 'react-chartjs-2';
 import getData from "../components/getData";
 
 ChartJS.register( CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend );
 
-const randomColor = () => {
-  return Math.floor(Math.random()*16777215).toString(16);
-}
-
 const numTachas = 21;
 const labels = [];
-labels.push(getData('temperatura', 1)[0]);
-
 const temps = [];
 const hums = [];
 const acels = [];
 
-['temperatura', 'humedad', 'aceleracion'].forEach((param) => {
-    for (let m = 1; m <= numTachas; m++) {
-        if (param === 'temperatura') {
-            temps.push({ 
-                data : getData(param, m)[1],
-                label : `Tacha ${m}`,
-                borderColor : '#' + randomColor(),
-                backgorundColor : '#' + randomColor()
-            });
-        }
-        if (param === 'humedad') {
-            hums.push({ 
-                data : getData(param, m)[1],
-                label : `Tacha ${m}`,
-                borderColor : '#' + randomColor(),
-                backgorundColor : '#' + randomColor()
-            });
-        }
-        if (param === 'aceleracion') {
-            acels.push({ 
-                data : getData(param, m)[1],
-                label : `Tacha ${m}`,
-                borderColor : '#' + randomColor(),
-                backgorundColor : '#' + randomColor()
-            });
-        }
-    }
-})
+const randomColor = () => {
+  return Math.floor(Math.random()*16777215).toString(16);
+}
+
+function obtenerDatos() {
+  console.log('Llamado a obtener datos');
+
+  labels.push(getData('temperatura', 1)[0]);
+
+  ['temperatura', 'humedad', 'aceleracion'].forEach((param) => {
+      for (let m = 1; m <= numTachas; m++) {
+          if (param === 'temperatura') {
+              temps.push({ 
+                  data : getData(param, m)[1],
+                  label : `Tacha ${m}`,
+                  borderColor : '#' + randomColor(),
+                  backgorundColor : '#' + randomColor()
+              });
+          }
+          if (param === 'humedad') {
+              hums.push({ 
+                  data : getData(param, m)[1],
+                  label : `Tacha ${m}`,
+                  borderColor : '#' + randomColor(),
+                  backgorundColor : '#' + randomColor()
+              });
+          }
+          if (param === 'aceleracion') {
+              acels.push({ 
+                  data : getData(param, m)[1],
+                  label : `Tacha ${m}`,
+                  borderColor : '#' + randomColor(),
+                  backgorundColor : '#' + randomColor()
+              });
+          }
+      }
+  })
+}
 
 const GlobalLineChartPlot = (props) => {
 
   let dataset = [];
   let title = '';
+
+  useEffect(() => {
+    setInterval(() => { obtenerDatos() }, 5000);
+  }, []);
 
   if (props.tempSel) {
     dataset = temps;
@@ -80,9 +88,11 @@ const GlobalLineChartPlot = (props) => {
   };
   
   const data = {
-    labels : labels[0].slice(props.inicioFecha, props.finalFecha),
-    datasets: dataset.slice(props.inicioTachas, props.finalTachas),
+    labels : labels[0],
+    datasets : dataset,
   };
+
+  console.log(data);
 
   return(
       <Line options={options} data={data}/>
