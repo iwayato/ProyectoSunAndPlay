470e662f3030a752d4d5b43961e6eded0fec5804
iwayato <iwayatomoaki@gmail.com>
2022-02-25 15:08:07 -0300

    Attempt to make DataPlot.js works

--- src/Graphs/DataPlot.js
+++ src/Graphs/DataPlot.js
@@ -4,6 +4,8 @@ import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement
 import { Line } from 'react-chartjs-2';
 import axios from 'axios';
 
+ChartJS.register( CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend );
+
 const DataPlot = () => {
 
     const [temps , setTemps] = useState([]);
@@ -12,28 +14,45 @@ const DataPlot = () => {
 
     const refresh = () => {
 
-        ['M_Temperatura', 'M_Humedad', 'M_Aceleracion'].forEach((param) => {
-
+        ['M_Temperatura', 'M_Aceleracion', 'M_Humedad'].forEach((param) => {
             axios.get(`https://proyecto-sun-and-play-server.herokuapp.com/data/${param}/1/true`).then((response) => {
+                for (var tacha in response.data.data) {
+                    for (var fecha in response.data.data[tacha]) {
+                        if (response.data.data[tacha].hasOwnProperty(fecha))
+                            if (fecha !== 'id') {
+                                if (param === 'M_Temperatura') {
+                                    // eslint-disable-next-line
+                                    setTemps(temps => [...temps, response.data.data[tacha][fecha]]);
+                                }
+                                if (param === 'M_Aceleracion') {
+                                    // eslint-disable-next-line
+                                    setAcels(acels => [...acels, response.data.data[tacha][fecha]]);
+                                }
+                                if (param === 'M_Humedad') {
+                                    // eslint-disable-next-line
+                                    setHums(hums => [...hums, response.data.data[tacha][fecha]]);
+                                }
+                            }
 
-                console.log(param, response.data.data);
-        
+                    }
+                }
             })
-
         })
-
     }
 
-    useEffect(refresh);
-        	
-    console.log(refresh());
+    useEffect(refresh, []);
+
+    console.log(temps);
 
     return(
         <div>
+            <br></br>
             <Link to="/" style={{ textDecoration: 'none' }}>Página principal</Link>
             <br></br>
             <br></br>
-            aksdjhaksjhd
+            <button onClick={refresh}>
+                Actualizar datos
+            </button>
         </div>
     )
 
